e39d1c1974262f6a6343a16301025f42c2eeeb4e
JenTing Hsiao <jenting.hsiao@suse.com>
2020-07-27 15:25:55 +0800

    Add velero helm3 install + minio
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

---
+++ helm3-install-velero-minio.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+helm3 repo add vmware-tanzu https://vmware-tanzu.github.io/helm-charts
+
+cat <<EOF < credentials-velero
+[default]
+aws_access_key_id = minio
+aws_secret_access_key = minio123
+EOF
+
+helm3 install velero \
+    --create-namespace \
+    --set-file credentials.secretContents.cloud=credentials-velero \
+    --set configuration.provider=aws \
+    --set configuration.backupStorageLocation.name=default \
+    --set configuration.backupStorageLocation.bucket=velero \
+    --set configuration.backupStorageLocation.config.region=minio \
+    --set configuration.backupStorageLocation.config.s3ForcePathStyle=true \
+    --set configuration.backupStorageLocation.config.s3Url=http://minio.velero.svc.cluster.local:9000 \
+    --set snapshotsEnabled=true \
+    --set deployRestic=true \
+    --set configuration.volumeSnapshotLocation.name=default \
+    --set configuration.volumeSnapshotLocation.config.region=minio \
+    --set initContainers[0].name=velero-plugin-for-aws \
+    --set initContainers[0].image=velero/velero-plugin-for-aws:v1.1.0 \
+    --set initContainers[0].volumeMounts[0].mountPath=/target \
+    --set initContainers[0].volumeMounts[0].name=plugins \
+    vmware-tanzu/velero
