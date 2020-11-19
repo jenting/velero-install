5fdd615cd1b94973ddb4032391628063b6997b2b
JenTing Hsiao <jenting.hsiao@suse.com>
2020-11-19 15:57:14 +0800

    Add flux example
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

---
+++ velero/flux.sh
@@ -0,0 +1,61 @@
+cat <<EOF | kubectl apply -f -
+apiVersion: helm.fluxcd.io/v1
+kind: HelmRelease
+metadata:
+  name: velero
+  namespace: velero
+  annotations:
+    fluxcd.io/automated: "false"
+spec:
+  releaseName: velero
+  chart:
+    repository: https://vmware-tanzu.github.io/helm-charts
+    name: velero
+    version: 2.13.6
+  values:
+    image:
+      repository: velero/velero
+      tag: v1.5.2
+    configuration:
+      backupStorageLocation:
+        bucket: velero
+        name: default
+        config:
+          region: minio-default
+          s3ForcePathStyle: true
+          s3Url: http://minio-default.velero.svc.cluster.local:9000
+          publicUrl: http://localhost:9000
+        accessMode: ReadWrite
+      provider: aws
+      resticTimout: 4h
+      volumeSnapshotLocation:
+        name: default
+    credentials:
+      existingSecret: cloud-credentials
+      useSecret: true
+    deployRestic: false
+    initContainers:
+    - image: velero/velero-plugin-for-aws:v1.1.0
+      imagePullPolicy: IfNotPresent
+      name: velero-plugin-for-aws
+      volumeMounts:
+      - mountPath: /target
+        name: plugins
+    metrics:
+      enabled: true
+      scrapeInterval: 30s
+      serviceMonitor:
+        additionalLabels:
+          prometheus: kube-prometheus
+        enabled: true
+    schedules:
+      backup-volumes:
+        schedule: '@every 24h'
+        template:
+          ttl: 720h
+          snapshotVolumes: true
+          storageLocation: default
+    serviceAccount:
+      server:
+        name: velero
+EOF
